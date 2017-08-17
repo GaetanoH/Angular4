96ec87c8ae435f4cdd7a66100d2cf982614d0c0e
Gaetano Herman <gaetano.herman.u9781@student.hogent.be>
2017-08-17 20:33:11 +0200

    Directive gemaakt voor openen sluiten van de dropdown

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,8 +1,15 @@
-import {Directive} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener} from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]',
 })
 export class DropdownDirective {
+  @HostBinding('class.open') isOpen = false;
 
+  constructor() {
+  }
+
+  @HostListener('click') toggleOpen() {
+    this.isOpen = !this.isOpen;
+  }
 }
